157784f3434b8f4fcca34a2b6bdf55cfeb05cac5
CesarTG <cesartorres94t@gmail.com>
2022-11-03 22:20:11 -0300

    funcionalidad javaScript terminada

--- tickets.js
+++ tickets.js
@@ -1,21 +1,23 @@
-document.getElementById("estudiante").addEventListener("click", estudiante)
 
+let x = document.getElementById("cantidad");
+let precio = 200
+
+document.getElementById("estudiante").addEventListener("click", estudiante)
 function estudiante () {
-    document.getElementById("total").innerHTML = ("Estudiante")
-    console.log(x * precio)
+    document.getElementById("total").innerHTML = ("$ " + x.value * precio * 0.80)    
 }
 
 document.getElementById("trainee").addEventListener("click", trainee)
-
 function trainee () {
-    document.getElementById("total").innerHTML = ("Trainee")
+    document.getElementById("total").innerHTML = ("$ " + x.value * precio * 0.50)    
 }
 
 document.getElementById("junior").addEventListener("click", junior)
-
 function junior () {
-    document.getElementById("total").innerHTML = ("Junior")
+    document.getElementById("total").innerHTML = ("$ " + x.value * precio * 0.15)    
 }
 
-x = document.getElementById("cantidad").value;
-let precio = 200
+document.getElementById("reset").addEventListener("click", borrar)
+function borrar () {
+    document.getElementById("total").innerHTML = ("")
+}
